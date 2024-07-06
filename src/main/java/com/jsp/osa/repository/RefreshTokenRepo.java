a2079e165c9132a009fa6c17a1eb6c538aaa518f
sharathkumarh10 <sharathkumarh1402@gmail.com>
2024-07-06 09:25:12 +0530

    schedulers are enabled to remove the expired tokens

--- src/main/java/com/jsp/osa/repository/RefreshTokenRepo.java
+++ src/main/java/com/jsp/osa/repository/RefreshTokenRepo.java
@@ -1,5 +1,6 @@
 package com.jsp.osa.repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,12 +11,12 @@ import com.jsp.osa.entity.User;
 
 public interface RefreshTokenRepo extends JpaRepository<RefreshToken, Integer> {
 
-
-
 	Optional<RefreshToken> findByRefreshToken(String refreshToken);
 
 	List<RefreshToken> findByUserAndIsBlocked(User user, boolean b);
 
 	List<RefreshToken> findByUserAndIsBlockedAndRefreshTokenNot(User user, boolean b, String refreshToken);
+
+	List<RefreshToken> findAllByExpirationBefore(LocalDateTime now);
 
 }
